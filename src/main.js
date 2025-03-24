144a693676c2aaf8d35856370524e67cefbac35d
Margot F <togramf@live.fr>
2025-03-24 13:20:25 +0100

    Updates

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import i18n from './i18n';
-import '@/assets/styles/main.css';
-
-
-const app = createApp(App);
-app.use(i18n);
-app.mount('#app');
